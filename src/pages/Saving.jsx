25c8c4dca32e5c35a29d2a768fcbe9cfbca1b1ce
KaviRathnayake <kavindyarathnayak@gmail.com>
2025-03-17 22:36:44 +0530

    feat: Add Saving component

---
+++ src/pages/Saving.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Saving = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Debt DebtMates</h1>
+      <p className="mt-4 text-lg">This is the Debt management home page.</p>
+    </div>
+    )
+}
+
+export default Saving
