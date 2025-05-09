db16d16b3c0a8d8c6b1ded552662b80be70efaae
Dhanithya Beligolla <it22085580@my.sliit.lk>
2025-05-10 00:05:31 +0530

    fix: Comment out Toaster component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import { Toaster } from 'react-hot-toast';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Toaster position="top-right" />
+    {/* <Toaster position="top-right" /> */}
   </React.StrictMode>
 );
